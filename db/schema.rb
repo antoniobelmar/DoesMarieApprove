e522bce9700aa91fda1871dd5a43426a39d0c0c0
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-12-12 15:23:55 +0000

    Added comment to review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212151022) do
+ActiveRecord::Schema.define(version: 20171212152047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20171212151022) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "restaurant_id"
+    t.text "comment"
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
