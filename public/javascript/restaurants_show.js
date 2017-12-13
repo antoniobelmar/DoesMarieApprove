304b4caf19a0e21e1b0d4d408c05821db807f2f7
thatdania@gmail.com <thatdania@gmail.com>
2017-12-13 16:07:24 +0000

    create javascript file for show page

---
+++ public/javascript/restaurants_show.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+  var id = window.location.pathname.split('/')[2]
+
+  $.get("/api/restaurants/"+ id, function(data){
+    $('#single-restaurant').append('<div id=restaurant-'+ id + '></div>')
+    $('#restaurant-' + id).append('<h1>' + data.restaurant.name + '</h1>')
+    $('#restaurant-' + id).append('<h3>' + data.restaurant.location + '</h3>')
+    $('#restaurant-' + id).append('<h4>' + data.restaurant.description + '</h4>')
+    $('#restaurant-' + id).append('<img src="' + data.restaurant.image + '">')
+  })
+
+  $('#form-page').append('<form action="/api/restaurants/'+ id +'/reviews" method="post"><input type="text" name="review[rating]"><input type="text" name="review[comment]"><input type="submit" value="Add Review"></form>')
+  // $('#form-page').append('<input type="text" name="rating">')
+  // $('#form-page').append('<input type="text" name="comment">')
+  // $('#form-page').append('<input type="submit" value="Add Review">')
+  // $('#form-page').append('</form>')
+
+});
