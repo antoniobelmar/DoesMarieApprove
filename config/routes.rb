05f0db6e364340fdcff997aa2e7e65ccc52ba748
antonio <antoniobelmardacosta@gmail.com>
2017-12-13 12:02:37 +0000

    Generate user controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   root "homepage#index"
   
   resources :restaurants
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212161326) do
+ActiveRecord::Schema.define(version: 20171213115437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20171212161326) do
     t.bigint "restaurant_id"
     t.text "comment"
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "reviews", "restaurants"
