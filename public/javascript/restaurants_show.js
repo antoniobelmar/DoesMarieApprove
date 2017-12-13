6bf6fe4703f60b55dc1a97fe6e014d749e3b57a6
thatdania@gmail.com <thatdania@gmail.com>
2017-12-13 16:16:07 +0000

    Show reviews on show page

--- public/javascript/restaurants_show.js
+++ public/javascript/restaurants_show.js
@@ -8,8 +8,18 @@ $(document).ready(function(){
     $('#restaurant-' + id).append('<h3>' + data.restaurant.location + '</h3>')
     $('#restaurant-' + id).append('<h4>' + data.restaurant.description + '</h4>')
     $('#restaurant-' + id).append('<img src="' + data.restaurant.image + '">')
+  });
+
+  $.get("/api/restaurants/" + id + "/reviews",function(data){
+    $('#single-restaurant').append('<h2> Reviews </h2>')
+    data.reviews.forEach(function(review){
+      $('#single-restaurant').append('<h5>' + review.rating + '</h5>')
+      $('#single-restaurant').append('<p>' + review.comment + '</p>')
+
+    });
   })
 
   $('#form-page').append('<form action="/api/restaurants/'+ id +'/reviews" method="post"><input type="text" name="review[rating]"><input type="text" name="review[comment]"><input type="submit" value="Add Review"></form>')
+
 
 });
