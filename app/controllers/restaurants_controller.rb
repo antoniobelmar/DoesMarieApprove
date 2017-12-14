851f78c9e9cb9a69768351956aad73525fbb68ca
antonio <antoniobelmardacosta@gmail.com>
2017-12-14 16:09:33 +0000

    Creates post route to create restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,24 @@
 class RestaurantsController < ApplicationController
+  protect_from_forgery except: :create
 
   def index
 
   end
 
+  def new
+
+  end
+
+  def create
+    # restaurant = Restaurant.new(name: params[:name], description: params[:description], image: params[:image], location: params[:location])
+    current_user.restaurants.create(name: params[:name], description: params[:description], image: params[:imageurl], location: params[:location])
+    # current_user.save!
+    render 'index'
+  end
+
   def show
 
-  end 
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :create, :show, :new]
 
   resources :users, only: [:new, :create]
 
