546fc23193a08263f4c34149ed5fc0518bd27eb3
Allan Wazacz <cazwazacz@gmail.com>
2017-12-14 14:25:48 +0000

    Add association => user has many restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213150656) do
+ActiveRecord::Schema.define(version: 20171214125057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20171213150656) do
     t.string "location"
     t.string "description"
     t.string "image"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -42,5 +44,6 @@ ActiveRecord::Schema.define(version: 20171213150656) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "restaurants", "users"
   add_foreign_key "reviews", "restaurants"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-restaurant1 = Restaurant.create(name:'Antonios',description:'sells soups',image:'http://google.com',location:'London')
-restaurant1.reviews.create(comment:'Always a good soup',rating: 4)
+ActiveRecord::Base.connection.tables.each do |table|
+    if table != 'schema_migrations'
+        ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table} CASCADE;")
+        ActiveRecord::Base.connection.reset_pk_sequence! table
+    end
+end
 
 
-restaurant2 = Restaurant.create(name:'Allans',description:'sells steaks',image:'http://google.com',location:'London')
-restaurant2.reviews.create(comment:'All right steak',rating: 2)
+user = User.create(name: 'Antonio', email: 'antonio123@antonio123.com', password: '123456', password_confirmation: '123456')
+user.restaurants.create(name:'Antonios',description:'sells soups',image:'https://media-cdn.tripadvisor.com/media/photo-s/0a/7b/7b/c6/restaurant-view.jpg',location:'London')
+user.restaurants.create(name:'Allans',description:'sells steaks',image:'https://www.tivolihotel.com/uploads/tx_templavoila/720x469-Tivoli-Hotel-restaurant-tivoli-brasserie-breakfast-5.jpg',location:'London')
+user.restaurants.create(name:'Marie',description:'does bottomless brunches',image:'https://www.greenbank-hotel.co.uk/wp-content/uploads/2016/12/waters-edge-restaurant-falmouth-restaurants-greenbank-hotel-cornwall-cosy-booths.jpg',location:'London')
+user.restaurants.create(name:'Dania',description:'specializes in chicken rice',image:'https://media-cdn.tripadvisor.com/media/photo-o/06/45/0e/fe/fino-cocktail-bar-restaurant.jpg',location:'London')
 
-restaurant3 = Restaurant.create(name:'Marie',description:'does bottomless brunches',image:'http://google.com',location:'London')
-restaurant3.reviews.create(comment:'Getting drunk with brunch',rating: 5)
+p Restaurant.all
 
 
-restaurant4 = Restaurant.create(name:'Dania',description:'specializes in chicken rice',image:'http://google.com',location:'London')
-restaurant4.reviews.create(comment:'Never a dissapointment',rating: 4)
-restaurant4.reviews.create(comment:'Best chicken rice in London',rating: 4)
+# restaurant1 = Restaurant.create(name:'Antonios',description:'sells soups',image:'http://google.com',location:'London')
+# restaurant1.reviews.create(comment:'Always a good soup',rating: 4)
+#
+#
+# restaurant2 = Restaurant.create(name:'Allans',description:'sells steaks',image:'http://google.com',location:'London')
+# restaurant2.reviews.create(comment:'All right steak',rating: 2)
+#
+# restaurant3 = Restaurant.create(name:'Marie',description:'does bottomless brunches',image:'http://google.com',location:'London')
+# restaurant3.reviews.create(comment:'Getting drunk with brunch',rating: 5)
+#
+#
+# restaurant4 = Restaurant.create(name:'Dania',description:'specializes in chicken rice',image:'http://google.com',location:'London')
+# restaurant4.reviews.create(comment:'Never a dissapointment',rating: 4)
+# restaurant4.reviews.create(comment:'Best chicken rice in London',rating: 4)
