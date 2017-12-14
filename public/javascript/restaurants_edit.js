95b0b03daaefb66d47e7c89f63a4a96f99c0db5f
thatdania@gmail.com <thatdania@gmail.com>
2017-12-14 17:10:31 +0000

    Add back button

--- public/javascript/restaurants_edit.js
+++ public/javascript/restaurants_edit.js
@@ -1,16 +1,17 @@
 
-  console.log(window.location.pathname)
 
   $(document).ready(function(){
     var id = window.location.pathname.split('/')[2]
 
     $("#Submit").on("click", function(){
       var name = $("#name").val();
-      console.log(name)
       $.post("/restaurants" , {name: name, id: id})
      window.location.href = "http://localhost:3000/restaurants/"
     });
 
+    $("#Back").on("click", function(){
+     window.location.pathname = "/restaurants/" + id 
+    });
 
 
   });
